3b92bd9753590a9cc91bdb91993cc0f5d934cb6b
Silvana <silvana15_91@hotmail.com>
2019-09-12 16:59:10 -0500

    Se agregaron notas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import Note from './Note/Note';
+import NoteForm from './NoteForm/NoteForm';
+
+class App extends React.Component{
+
+  constructor() {
+    super();
+    this.state = {
+      notes: [
+        {noteId: 1, noteContent: 'note1'},
+        {noteId: 2, noteContent: 'note2'},
+      ]
+    };
+    this.addNote = this.addNote.bind(this);
+  }
+
+  removeNote() {
+
+  }
+
+  addNote(note) {
+    let { notes } = this.state;
+    notes.push({
+      noteId: note.length + 1,
+      noteContent: note
+    });
+    this.setState({ notes });
+
+  }
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola Laboratoria!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="notesContainer">
+        
+          <div className="notesHeader">
+            <h1>Scary Notes</h1>
+          </div>
+
+          <div className="notesBody">
+          <ul>
+          {
+            
+              this.state.notes.map(note =>{
+                return(
+                  <Note
+                  noteContent={note.noteContent}
+                  noteId={note.noteId}
+                  key={note.noteId}
+                  />
+                )
+              })
+              
+          }
+          </ul>
+           
+          </div>
+
+          <div className="notesFooter">
+            <NoteForm addNote={this.addNote}/>
+          </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
