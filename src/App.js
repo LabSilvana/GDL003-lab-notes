7008f48c93b3eb9a6973d828fcb45285ab2e972a
Silvana <silvana15_91@hotmail.com>
2019-09-13 12:41:56 -0500

    Se agrego firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
 
+import firebase from 'firebase';
+import { DB_CONFIG } from './config/config';
+import 'firebase/database';
+
 import Note from './Note/Note';
 import NoteForm from './NoteForm/NoteForm';
 
@@ -10,11 +14,26 @@ class App extends React.Component{
     super();
     this.state = {
       notes: [
-        {noteId: 1, noteContent: 'note1'},
-        {noteId: 2, noteContent: 'note2'},
+        //{noteId: 1, noteContent: 'note1'},
+        //{noteId: 2, noteContent: 'note2'},
       ]
     };
+
+    this.app = firebase.initializeApp(DB_CONFIG);
+    this.db = this.app.database().ref().child('notes');
+
     this.addNote = this.addNote.bind(this);
+  }
+
+  componentDidMount() {
+    const { notes } = this.state;
+    this.db.on('child_added', snap => {
+      notes.push({
+        noteId: snap.key,
+        noteContent: snap.val().noteContent
+      })
+      this.setState({notes});
+    });
   }
 
   removeNote() {
@@ -22,13 +41,15 @@ class App extends React.Component{
   }
 
   addNote(note) {
-    let { notes } = this.state;
+    /*let { notes } = this.state;
     notes.push({
       noteId: note.length + 1,
       noteContent: note
     });
-    this.setState({ notes });
+    this.setState({ notes });*/
 
+    this.db.push().set({noteContent: note});
+     
   }
 
   render() {
