f14305fb9e1d8a40a85c0f52562fcb4393cc7375
Silvana <silvana15_91@hotmail.com>
2019-11-26 19:36:23 -0600

    Updates

---
+++ precache-manifest.2769e31b95c17fbd2c625a3ba3cd842f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b7ebfa965170411b5caac5f4078f4e7",
+    "url": "/LabSilvana/GDL003-lab-notes/index.html"
+  },
+  {
+    "revision": "ecd79afcc890754f852f",
+    "url": "/LabSilvana/GDL003-lab-notes/static/css/main.f2f602a4.chunk.css"
+  },
+  {
+    "revision": "d59b1a42366f9e961a56",
+    "url": "/LabSilvana/GDL003-lab-notes/static/js/2.5687f250.chunk.js"
+  },
+  {
+    "revision": "ecd79afcc890754f852f",
+    "url": "/LabSilvana/GDL003-lab-notes/static/js/main.1133740f.chunk.js"
+  },
+  {
+    "revision": "055a4a6d9ddabd053076",
+    "url": "/LabSilvana/GDL003-lab-notes/static/js/runtime~main.7d8be815.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3ce7892fce206b0e5feb2638109ca162.js"
+  "/LabSilvana/GDL003-lab-notes/precache-manifest.2769e31b95c17fbd2c625a3ba3cd842f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/LabSilvana/GDL003-lab-notes/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
